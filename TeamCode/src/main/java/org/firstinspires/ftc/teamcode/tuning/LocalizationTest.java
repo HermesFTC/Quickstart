d1c10ad4a3164733e235701c0f3f3ec825a988ab
Zach Harel <zach@zharel.me>
2025-10-14 11:59:19 -0400

    add flight logging to LocalizationTest for pose tracking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/LocalizationTest.java
@@ -5,11 +5,14 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import gay.zharel.fateweaver.flight.FlightLogChannel;
+import gay.zharel.fateweaver.flight.FlightRecorder;
 import gay.zharel.hermes.actions.Drawing;
 import gay.zharel.hermes.ftc.MecanumDrive;
 import gay.zharel.hermes.geometry.Pose2d;
 import gay.zharel.hermes.geometry.PoseVelocity2d;
 import gay.zharel.hermes.geometry.Vector2d;
+import gay.zharel.hermes.logs.PoseMessage;
 import org.firstinspires.ftc.teamcode.MecanumDriveFactory;
 
 public class LocalizationTest extends LinearOpMode {
@@ -18,6 +21,7 @@ public class LocalizationTest extends LinearOpMode {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         MecanumDrive drive = MecanumDriveFactory.build(hardwareMap, new Pose2d(0, 0, 0));
+        FlightLogChannel<PoseMessage> poseChannel = FlightRecorder.createChannel("poses", PoseMessage.class);
 
         waitForStart();
 
@@ -37,6 +41,9 @@ public class LocalizationTest extends LinearOpMode {
             telemetry.addData("y", pose.position.y);
             telemetry.addData("heading (deg)", Math.toDegrees(pose.heading.toDouble()));
             telemetry.update();
+
+            poseChannel.put(new PoseMessage(pose));
+            FlightRecorder.timestamp();
 
             TelemetryPacket packet = new TelemetryPacket();
             packet.fieldOverlay().setStroke("#3F51B5");
